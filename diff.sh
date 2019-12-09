c7fd99b1e6e66f7528635d7a405d673fb6157066
Ryan-Ng <617104810@qq.com>
2019-12-09 23:31:43 +0800

    Update diff.sh

--- diff.sh
+++ diff.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd openwrt 
 echo "src-git lienol https://github.com/Lienol/openwrt-package" >> feeds.conf.default
-cd package 
-git clone https://github.com/Ameykyl/luci-app-ssr-plus-jo
-git clone https://github.com/Ameykyl/my
+git clone https://github.com/Ameykyl/luci-app-ssr-plus-jo package 
+git clone https://github.com/Ameykyl/my package
 cd ..
