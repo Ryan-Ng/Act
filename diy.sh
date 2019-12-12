21a75f81cce42465912fa17955a075103d77dfa1
Ryan-Ng <617104810@qq.com>
2019-12-12 17:57:09 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,5 +9,5 @@
 sed -i 's/192.168.1.1/192.168.1.233/g' package/base-files/files/bin/config_generate
 #echo "src-git lienol https://github.com/Lienol/openwrt-package" >> feeds.conf.default
 cd package
-git clone https://github.com/Ameykyl/luci-app-ssr-plus-jo
-git clone https://github.com/Ameykyl/my
+git clone https://github.com/Ryan-Ng/luci-app-ssr-plus-jo
+git clone https://github.com/Leo-Jo/my
