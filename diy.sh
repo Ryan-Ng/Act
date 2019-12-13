e0e0220b6c306c36ce50b8ffa9c5815d92544388
Ryan-Ng <617104810@qq.com>
2019-12-13 15:47:17 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,6 +8,9 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.233/g' package/base-files/files/bin/config_generate
 #echo "src-git lienol https://github.com/Lienol/openwrt-package" >> feeds.conf.default
+# cat >> .config << EOF
+# CONFIG_PACKAGE_
+#EOF
 cd package
 git clone https://github.com/Ryan-Ng/luci-app-ssr-plus-jo
 git clone https://github.com/Ryan-Ng/my
